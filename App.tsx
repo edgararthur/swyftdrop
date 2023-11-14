98d5b4ff057d82a7bf67024b8acb900b121cfe6b
edgararthur <gilfoyle.arthur@protonmail.com>
2023-11-14 12:04:42 +0000

    invoke Main component

--- App.tsx
+++ App.tsx
@@ -3,9 +3,37 @@ import { StyleSheet, Text, View } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React, { useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import SplashScreen from "expo-splash-screen";
+
+import * as Font from "expo-font";
 import Main from "./Main";
 
+const fetchFonts = () => {
+	return Font.loadAsync({
+		poppins: require("./assets/fonts/Poppins-Regular.ttf"),
+	});
+};
+
 export default function App() {
+	const [fontsLoaded, setFontsLoaded] = useState(false);
+
+	useEffect(() => {
+		const loadFonts = async () => {
+			await fetchFonts();
+			setFontsLoaded(true);
+
+			if (SplashScreen && SplashScreen.hideAsync) {
+				SplashScreen.hideAsync();
+			}
+		};
+		loadFonts();
+	}, []);
+
+	if (!fontsLoaded) {
+		return null;
+	}
+	const Stack = createNativeStackNavigator();
+
 	return <Main />;
 }
 
