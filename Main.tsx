b0b6c3127fe93ec6f70c093ec41c5b874efe466e
edgararthur <gilfoyle.arthur@protonmail.com>
2023-11-14 12:06:22 +0000

    modified navigation

--- Main.tsx
+++ Main.tsx
@@ -1,49 +1,51 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import React, { useEffect, useState } from "react";
-import { NavigationContainer } from "@react-navigation/native";
+import React, { useEffect } from "react";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import Onboarding from "./src/screens/Onboarding";
 import Carousel from "./src/screens/Carousel";
 import Welcome from "./src/screens/Welcome";
-import Signup from "./src/screens/Signup";
-import VerifyPhone from "./src/screens/VerifyPhone";
 
 const Stack = createNativeStackNavigator();
 
-export default function Main() {
-	const [screen, setShowScreen] = useState("Onboarding");
+export default function App() {
+	return (
+		<NavigationContainer>
+			<Main />
+		</NavigationContainer>
+	);
+}
+
+function Main() {
+	const navigation = useNavigation();
 
 	useEffect(() => {
 		const timer = setTimeout(() => {
-			setShowScreen("Carousel");
+			navigation.navigate("Carousel");
 		}, 5000);
 
 		return () => clearTimeout(timer);
-	}, []);
+	}, [navigation]);
 
 	return (
-		<NavigationContainer>
-			<Stack.Navigator>
-				{/* <Stack.Screen
-					name="Onboarding"
-					component={Onboarding}
-					options={{ headerShown: false }}
-				/>
-				{screen === "Carousel" && (
-					<Stack.Screen
-						name="Carousel"
-						component={Carousel}
-						options={{ headerShown: false }}
-					/>
-				)} */}
-				<Stack.Screen
-					name="Carousel"
-					component={VerifyPhone}
-					options={{ headerShown: false }}
-				/>
-			</Stack.Navigator>
-		</NavigationContainer>
+		<Stack.Navigator>
+			<Stack.Screen
+				name="Onboarding"
+				component={Onboarding}
+				options={{ headerShown: false }}
+			/>
+			<Stack.Screen
+				name="Carousel"
+				component={Carousel}
+				options={{ headerShown: false }}
+			/>
+			<Stack.Screen
+				name="Welcome"
+				component={Welcome}
+				options={{ headerShown: false }}
+			/>
+		</Stack.Navigator>
 	);
 }
 
