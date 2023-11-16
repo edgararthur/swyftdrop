17bad60a0b072263e99f44ba15b14605bc402ae5
edgararthur <gilfoyle.arthur@protonmail.com>
2023-11-16 11:34:10 +0000

    refactor handleSignup function

--- src/screens/Signup.tsx
+++ src/screens/Signup.tsx
@@ -1,64 +1,133 @@
-import React from "react";
+import React, { useState } from "react";
 import {
 	View,
 	Text,
 	StyleSheet,
 	StatusBar,
 	TouchableOpacity,
+	Alert,
+	SafeAreaView,
 } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 import AppInput from "../components/Inputs";
 import Checkbox from "../components/Checkbox";
 import AppButton from "../components/Buttons";
+import { useNavigation } from "@react-navigation/native";
 
-export default function Signup({ navigation }) {
+export default function Signup() {
+	const navigation = useNavigation();
+
 	const handleCheckboxChange = (checked: boolean) => {
 		console.log("Checkbox is checked:", checked);
 	};
 
+	const [fullName, setFullName] = useState("");
+	const [email, setEmail] = useState("");
+	const [phone, setPhoneNumber] = useState("");
+	const [password, setPassword] = useState("");
+	const [confirmPassword, setConfirmPassword] = useState("");
+
 	const navigateToSignIn = () => {
-		console.log("Navigating to Sign In");
+		(navigation.navigate as (routeName: string) => void)("Signin");
+	};
+
+	const handleSignUp = () => {
+		console.log(fullName, email, phone, password, confirmPassword);
+
+		// Reset the input fields after logging the values
+		setFullName("");
+		setEmail("");
+		setPhoneNumber("");
+		setPassword("");
+		setConfirmPassword("");
 	};
 
 	return (
-		<View style={styles.container}>
-			<StatusBar backgroundColor="#eef1fd" barStyle="dark-content" />
-			<View style={styles.header}>
-				<MaterialIcons name="chevron-left" size={36} color="#2a2a2a" />
-				<View style={styles.headerText}>
-					<Text style={styles.text}>Create Account</Text>
+		<SafeAreaView>
+			<View style={styles.container}>
+				{StatusBar.currentHeight !== undefined && (
+					<StatusBar
+						backgroundColor="#eef1fd"
+						barStyle="dark-content"
+					/>
+				)}
+				<View style={styles.header}>
+					<MaterialIcons
+						name="chevron-left"
+						size={36}
+						color="#2a2a2a"
+					/>
+					<View style={styles.headerText}>
+						<Text style={styles.text}>Create Account</Text>
+					</View>
+				</View>
+				<View style={styles.form}>
+					<AppInput
+						name="Full Name"
+						style={styles.input}
+						onChangeText={(text: React.SetStateAction<string>) =>
+							setFullName(text)
+						}
+						keyboardType="default"
+					/>
+					<AppInput
+						name="Email"
+						style={styles.input}
+						onChangeText={(email: React.SetStateAction<any>) =>
+							setEmail(email)
+						}
+						keyboardType="email-address"
+					/>
+					<AppInput
+						name="Phone Number"
+						style={styles.input}
+						onChangeText={(phone: React.SetStateAction<string>) =>
+							setPhoneNumber(phone)
+						}
+						keyboardType="phone-pad"
+					/>
+					<AppInput
+						name="Password"
+						style={styles.input}
+						onChangeText={(
+							password: React.SetStateAction<string>
+						) => setPassword(password)}
+						keyboardType="default"
+					/>
+					<AppInput
+						name="Confirm Password"
+						style={styles.input}
+						onChangeText={(
+							confirmPassword: React.SetStateAction<string>
+						) => setConfirmPassword(confirmPassword)}
+						keyboardType="default"
+					/>
+					<View>
+						<Checkbox
+							label="By signing up, you agree to our Terms of Service and Privacy Policy."
+							onChange={handleCheckboxChange}
+						/>
+					</View>
+					<View style={styles.submitBtn}>
+						<AppButton
+							title="Sign Up"
+							backgroundColor="primary"
+							textColor="white"
+							onPress={handleSignUp}
+						/>
+					</View>
+					<TouchableOpacity
+						onPress={navigateToSignIn}
+						style={styles.signInContainer}
+					>
+						<Text style={styles.signInLink}>
+							Already have an account?{" "}
+							<Text style={styles.signInText}>Sign in</Text>
+						</Text>
+					</TouchableOpacity>
 				</View>
 			</View>
-			<View style={styles.form}>
-				<AppInput name="Full Name" style={styles.input} />
-				<AppInput name="Email" style={styles.input} />
-				<AppInput name="Phone Number" style={styles.input} />
-				<AppInput name="Password" style={styles.input} />
-				<AppInput name="Confirm Password" style={styles.input} />
-				<View>
-					<Checkbox
-						label="By signing up, you agree to our Terms of Service and Privacy Policy."
-						onChange={handleCheckboxChange}
-					/>
-				</View>
-				<View style={styles.submitBtn}>
-					<AppButton
-						title="Sign Up"
-						backgroundColor="primary"
-						textColor="white"
-					/>
-				</View>
-				<TouchableOpacity
-					onPress={navigateToSignIn}
-					style={styles.signInContainer}
-				>
-					<Text style={styles.signInLink}>
-						Already have an account?{" "}
-						<Text style={styles.signInText}>Sign in</Text>
-					</Text>
-				</TouchableOpacity>
-			</View>
-		</View>
+		</SafeAreaView>
 	);
 }
 
@@ -106,12 +175,12 @@ const styles = StyleSheet.create({
 	signInLink: {
 		textAlign: "center",
 		color: "#2a2a2a",
-		fontSize: 15,
+		fontSize: 18,
 		fontWeight: "500",
 	},
 	signInText: {
 		color: "#1f6afe",
-		fontSize: 15,
+		fontSize: 18,
 		fontWeight: "500",
 	},
 });
